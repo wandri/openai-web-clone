5ecea8b93fef039bb4a180b60a3995886e2ed8ab
Wandrille <wandrille.guesdon@gmail.com>
2023-12-14 17:15:16 +0100

    Remove SSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideClientHydration} from '@angular/platform-browser';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {SvgIconModule} from "../shared/svg-icon.module";
 import {HttpClientModule} from "@angular/common/http";
@@ -10,7 +9,6 @@ import {HttpClientModule} from "@angular/common/http";
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
     provideAnimations(),
     importProvidersFrom(
       SvgIconModule,
