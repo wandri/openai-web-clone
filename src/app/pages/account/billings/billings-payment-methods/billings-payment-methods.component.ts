31cdebb6f58267a14f554a75e68d4dafc0ed85c3
Wandrille <wandrille.guesdon@gmail.com>
2023-12-22 14:53:25 +0100

    Add Profile settings

--- src/app/pages/account/billings/billings-payment-methods/billings-payment-methods.component.ts
+++ src/app/pages/account/billings/billings-payment-methods/billings-payment-methods.component.ts
@@ -1,13 +1,37 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ButtonTextComponent} from "../../../../components/elements/buttons/button-text/button-text.component";
 
 @Component({
   selector: 'app-billings-payment-methods',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonTextComponent
+  ],
   templateUrl: './billings-payment-methods.component.html',
   styles: [':host {@apply flex flex-col w-full h-full}'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BillingsPaymentMethodsComponent {
-
+  readonly cards: {
+    brand: string,
+    country: string,
+    expirationMonth: number,
+    expirationYear: number,
+    last4: string
+  }[] = [
+    {
+      brand: "mastercard",
+      country: "FR",
+      expirationMonth: 11,
+      expirationYear: 2024,
+      last4: "4764"
+    },
+    {
+      brand: "visa",
+      country: "FR",
+      expirationMonth: 1,
+      expirationYear: 2030,
+      last4: "5094"
+    }
+  ]
 }
--- src/app/pages/account/profile/profile.component.ts
+++ src/app/pages/account/profile/profile.component.ts
@@ -1,16 +1,31 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {HeaderComponent} from "../../../components/elements/headers/header/header.component";
+import {ButtonTextComponent} from "../../../components/elements/buttons/button-text/button-text.component";
+import {InputTextComponent} from "../../../components/elements/inputs/input-text/input-text.component";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {ButtonComponent} from "../../../components/elements/buttons/button/button.component";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    ButtonTextComponent,
+    InputTextComponent,
+    ReactiveFormsModule,
+    ButtonComponent
   ],
   templateUrl: './profile.component.html',
   styles: [':host {@apply flex flex-col w-full h-full}'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileComponent {
+  readonly nameForm = new FormControl<string>('John Doe');
+  readonly emailForm = new FormControl<string>('john.doe@company.com');
+  readonly phoneForm = new FormControl<string>('+33689403381');
 
+  constructor() {
+    this.emailForm.disable();
+    this.phoneForm.disable();
+  }
 }
