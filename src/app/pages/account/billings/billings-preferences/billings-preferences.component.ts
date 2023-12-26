8f978705d705c7816edc657f13a86de5958838ab
Wandrille <wandrille.guesdon@gmail.com>
2023-12-26 13:20:12 +0100

    Add billings preference

--- src/app/pages/account/billings/billings-preferences/billings-preferences.component.ts
+++ src/app/pages/account/billings/billings-preferences/billings-preferences.component.ts
@@ -1,13 +1,30 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {BadgeComponent} from "../../../../components/elements/tag/badge.component";
+import {ButtonTextComponent} from "../../../../components/elements/buttons/button-text/button-text.component";
+import {CdkTableModule} from "@angular/cdk/table";
+import {DatePipe} from "@angular/common";
+import {
+  SkeletonLoaderComponent
+} from "../../../../components/elements/loaders/skeleton-loader/skeleton-loader.component";
+import {InputTextComponent} from "../../../../components/elements/inputs/input-text/input-text.component";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-billings-preferences',
   standalone: true,
-  imports: [],
+  imports: [
+    BadgeComponent,
+    ButtonTextComponent,
+    CdkTableModule,
+    DatePipe,
+    SkeletonLoaderComponent,
+    InputTextComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './billings-preferences.component.html',
   styles: [':host {@apply flex flex-col w-full h-full}'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BillingsPreferencesComponent {
-
+  readonly companyNameForm = new FormControl('John Doe Corp.')
 }
